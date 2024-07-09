9a1c27b936709880f0758c5fd593a5763b481802
utsab <shakhawatutsab@gmail.com>
2024-07-09 15:52:46 +0600

    PHP switch example

--- index.php
+++ index.php
@@ -1,35 +1,54 @@
 <!-- if elseif basic -->
 <?php 
-    $num=122;  
-    if($num<100){  
-    echo "$num is less than 100";  
-}  elseif($num>100){
-    echo "$num is getter than 100"."</br>";
-}
+//     $num=122;  
+//     if($num<100){  
+//     echo "$num is less than 100";  
+// }  elseif($num>100){
+//     echo "$num is getter than 100"."</br>";
+// }
 ?>
 <!-- If Elseif basic Condition -->
 
 <?php  
-    $marks=80;      
-    if ($marks<33){    
-        echo "fail";    
-    }    
-    else if ($marks>=34 && $marks<50) {    
-        echo "Congratulation You GOt C";    
-    }    
-    else if ($marks>=50 && $marks<65) {    
-       echo "Congratulation You Got B grade";   
-    }    
-    else if ($marks>=65 && $marks<80) {    
-        echo "Congratulation You Got A";   
-    }    
-    else if ($marks>=80 && $marks<90) {    
-        echo "Congratulation You Got A+";    
-    }  
-    else if ($marks>=90 && $marks<100) {    
-        echo "Congratulation You Got Golden A+";   
-    }  
-   else {    
-        echo "Congratulation Invalid input";    
-    }    
+//     $marks=80;      
+//     if ($marks<33){    
+//         echo "fail";    
+//     }    
+//     else if ($marks>=34 && $marks<50) {    
+//         echo "Congratulation You GOt C";    
+//     }    
+//     else if ($marks>=50 && $marks<65) {    
+//        echo "Congratulation You Got B grade";   
+//     }    
+//     else if ($marks>=65 && $marks<80) {    
+//         echo "Congratulation You Got A";   
+//     }    
+//     else if ($marks>=80 && $marks<90) {    
+//         echo "Congratulation You Got A+";    
+//     }  
+//     else if ($marks>=90 && $marks<100) {    
+//         echo "Congratulation You Got Golden A+";   
+//     }  
+//    else {    
+//         echo "Congratulation Invalid input";    
+//     }    
+?>  
+
+<!-- PHP Switch Example -->
+
+<?php      
+    $num=15;      
+    switch($num){      
+    case 10:      
+    echo("number is equals to 10");      
+    break;      
+    case 20:      
+    echo("number is equal to 20");      
+    break;      
+    case 30:      
+    echo("number is equal to 30");      
+    break;      
+    default:      
+    echo("number is not equal to 10, 20 or 30");      
+    }     
 ?>  
